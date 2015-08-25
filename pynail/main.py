0cd421a32e178195775a2a4fee0975d71e7c3141
Tushar Gautam <tushar.rishav@gmail.com>
2015-08-25 23:28:29 +0530

    Minor fix

--- pynail/main.py
+++ pynail/main.py
@@ -8,7 +8,6 @@ from sys import argv, exit
 from multiprocessing.dummy import Pool as ThreadPool
 
 
-
 class Img:
 
     def __init__(self):
@@ -26,7 +25,7 @@ class Img:
                 logging.info("Thumbanails successfully created...")
             except Exception as e:
                 logging.error("%s Cause: Maybe invalid \
-                file format or wrong permission.",e)
+                file format or wrong permission.", e)
         else:
             if path.isdir(self.arg.cdir):
                 _ = listdir(self.arg.cdir)
@@ -46,39 +45,58 @@ class Img:
         f = self.arg.cdir.rstrip("/") + "/" + i
         if 'image' in magic.from_file(f, mime=True):  # extract all images
             if not path.isdir(self.arg.destination):
-                mkdir(self.arg.destination)         # if directory do not exists then create a new one
+                mkdir(self.arg.destination)
             try:
                 im_data = Image.open(f)
                 im_data.thumbnail(self.size, Image.ANTIALIAS)
-                __ = self.arg.destination.rstrip("/") + "/" + i[:-1*len(im_data.format)-1] + ".thumbnail"
+                __ = self.arg.destination.rstrip("/") + "/\
+                " + i[:-1*len(im_data.format)-1] + ".thumbnail"
                 im_data.save(__, self.arg.format)
             except Exception as e:
-                logging.error("%s Maybe invalid file format or wrong permission.",e)
+                logging.error("%s Maybe invalid file format or wrong\
+                permission.", e)
                 self.err = True
 
 
-def main():
-    logging.getLogger().name = "Pynail "
-    logging.getLogger().setLevel(logging.INFO)
-    Im = Img()
+def logger(func):
+    def log():
+        logging.getLogger().name = "Pynail "
+        logging.getLogger().setLevel(logging.INFO)
+        args = func()
+        return args
+    return log
+
+
+@logger
+def parse():
     parser = argparse.ArgumentParser(description=" \
-             Multithreaded quick image processing from terminal", epilog="Author:\
-              tushar.rishav@gmail.com")
+             Multithreaded quick image processing from terminal", epilog="\
+             Author:tushar.rishav@gmail.com")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-i", "--simg", help="Source path of image for thumbnail",
+    group.add_argument("-i", "--simg", help="Source path of image for \
+        thumbnail",
                        type=str)
     parser.add_argument("-t", "--timg", help="Target path for thumbnail",
                         type=str)
-    group.add_argument("-c", "--cdir", help="For a given directory path it converts all \
-        the images into thumbnail", type=str, default=curdir)
+    group.add_argument("-c", "--cdir", help="For a given directory path it\
+    converts all the images into thumbnail", type=str, default=curdir)
     parser.add_argument("-d", "--destination", help="Target directory path",
                         type=str, default=curdir)
-    parser.add_argument("-s", "--size", help="Size of the thumbnails", type=str, default="200,200")
-    parser.add_argument("-f", "--format", help="Format of the thumbnails", type=str, default="JPEG")
+    parser.add_argument("-s", "--size", help="Size of the thumbnails\
+        ", type=str, default="200,200")
+    parser.add_argument("-f", "--format", help="Format of the thumbnails\
+        ", type=str, default="JPEG")
     args = parser.parse_args()
     if len(argv) == 1:
         parser.print_help()
         exit(1)
+    return args
+
+
+def main():
+    Im = Img()
+    args = parse()
+    
     if args.simg:
         Im.thumb_gen(args, 'simg', 0)
     elif args.cdir:
